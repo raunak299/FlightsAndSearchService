2d7328d66dd97028b7d72606dfcbf37c5b08fb54
raunak299 <iamraunak299@gmail.com>
2024-07-05 15:26:46 +0530

    city router added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
 const CityRepository = require("./repository/city-repository");
 
@@ -12,10 +13,11 @@ const setupAndStartServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.use("/api", ApiRoutes);
+
   app.listen(PORT, () => {
     console.log(`server started at ${PORT} `);
     const repo = new CityRepository();
-    repo.createCity({ name: "New Delhi" });
   });
 };
 
