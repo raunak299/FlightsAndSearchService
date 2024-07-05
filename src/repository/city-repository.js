f8f60e92ded858ee3c86da0ee4d074b8c6c67c5c
raunak299 <iamraunak299@gmail.com>
2024-07-05 15:25:28 +0530

    city repository added

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,7 +6,7 @@ class CityRepository {
       const city = await City.create({ name });
       return city;
     } catch (err) {
-      throw new Error(err);
+      throw err;
     }
   }
 
@@ -17,8 +17,38 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (err) {
-      throw new Error(err);
+      throw err;
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
+      return city;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAllCities() {
+    try {
+      const cities = await City.findAll();
+      return cities;
+    } catch (err) {
+      throw err;
     }
   }
 }
