56518e0bf7b395e837700efc022b462e324efd21
raunak299 <iamraunak299@gmail.com>
2024-07-05 15:25:08 +0530

    city model update

--- src/models/city.js
+++ src/models/city.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class City extends Model {
     /**
@@ -13,15 +11,18 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  City.init({
-    name: {
-      type :  DataTypes.STRING,
-      allowNull:false,
-      unique:true
+  City.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "City",
     }
-  }, {
-    sequelize,
-    modelName: 'City',
-  });
+  );
   return City;
 };
